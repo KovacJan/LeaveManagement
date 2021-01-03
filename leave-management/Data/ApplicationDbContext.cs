ae2004c9bf815c151a5dd7715f427a49e619971f
unknown <jan.kovac@windowslive.com>
2021-01-03 21:46:54 +0100

    database setup, migrations, models

--- leave-management/Data/ApplicationDbContext.cs
+++ leave-management/Data/ApplicationDbContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace leave_management.Data
@@ -12,5 +9,13 @@ namespace leave_management.Data
             : base(options)
         {
         }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
+        public DbSet<LeaveHistory> LeaveHistories { get; set; }
     }
 }
