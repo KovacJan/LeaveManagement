6e0b4c64c18a7e771fa4b0acf0fb67cd829ad803
unknown <jan.kovac@windowslive.com>
2021-01-03 22:38:14 +0100

    repository pattern implementation with contracts

---
+++ leave-management/Repository/LeaveHistoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System.Collections.Generic;
+
+namespace leave_management.Repository
+{
+    public class LeaveHistoryRepository : ILeaveHistoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveHistoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool Create(LeaveHistory entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(LeaveHistory entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICollection<LeaveHistory> FindAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public LeaveHistory FindById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Update(LeaveHistory entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- leave-management/Startup.cs
+++ leave-management/Startup.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
+using leave_management.Contracts;
 using leave_management.Data;
+using leave_management.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +26,9 @@ namespace leave_management
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+            services.AddScoped<ILeaveHistoryRepository, LeaveHistoryRepository>();
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
