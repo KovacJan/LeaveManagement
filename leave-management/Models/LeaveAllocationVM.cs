3414232a219aa1700fe3ca6b069fa47a9e88c803
Jan Kovac <jan.kovac@windowslive.com>
2021-01-17 12:25:57 +0100

    database update

--- leave-management/Models/LeaveAllocationVM.cs
+++ leave-management/Models/LeaveAllocationVM.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace leave_management.Models
 {
@@ -9,10 +8,11 @@ namespace leave_management.Models
     {
         public int Id { get; set; }
 
-        [Required]
         public int NumberOfDays { get; set; }
 
         public DateTime DateCreated { get; set; }
+
+        public int Period { get; set; }
 
         public EmployeeVM Employee { get; set; }
 
