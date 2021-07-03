437318b0f7b561013ff4bb45ea1bdfd3d6defd1f
janci <jan.kovac@windowslive.com>
2021-07-03 21:29:37 +0200

    admin LTE template

--- leave-management/Controllers/HomeController.cs
+++ leave-management/Controllers/HomeController.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using leave_management.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace leave_management.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
